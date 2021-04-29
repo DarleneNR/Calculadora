40286f91e8c9c21fa9eda6aa6fbcb9b1416d1c0f
Darlene Navarro <dar.navarror@duocuc.cl>
2021-04-29 15:12:52 -0400

    Avance Calculadora 3

--- TaskCalculadora/funciones2.js
+++ TaskCalculadora/funciones2.js
@@ -1,95 +1,113 @@
 // Segunda Calculadora
 
-// Variables para la operaciones
-var number1;
-var number2;
-var type_operation;
+var result = document.getElementById("result");
 
-function inicio(){
-    // Obteniendo los datos de los botonones para convertirlos en elementos
-    var result = document.getElementById("result");
-    var clean = document.getElementById("clean");
-    var sum = document.getElementById("sum");
-    var subt = document.getElementById("subt");
-    var multiplication = document.getElementById("multiplication");
-    var div = document.getElementById("div");
-    var same = document.getElementById("same");
-    var one = document.getElementById("one");
-    var two = document.getElementById("two");
-    var three = document.getElementById("three");
-    var four = document.getElementById("four");
-    var five = document.getElementById("five");
-    var six = document.getElementById("six");
-    var seven = document.getElementById("seven");
-    var eight = document.getElementById("eight");
-    var nine = document.getElementById("nine");
-    var zero = document.getElementById("zero");
+function multiplication(){
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
 
-    // Eventos
-    // Al presionar el número este se mostrará
-    one.onclick = function(e){
-        result.textContent = result.textContent + "1";
-    }
-    two.onclick = function(e){
-        result.textContent = result.textContent + "2";
-    }
-    three.onclick = function(e){
-        result.textContent = result.textContent + "3";
-    }
-    four.onclick = function(e){
-        result.textContent = result.textContent + "4";
-    }
-    five.onclick = function(e){
-        result.textContent = result.textContent + "5";
-    }
-    six.onclick = function(e){
-        result.textContent = result.textContent + "6";
-    }
-    seven.onclick = function(e){
-        result.textContent = result.textContent + "7";
-    }
-    eight.onclick = function(e){
-        result.textContent = result.textContent + "8";
-    }
-    nine.onclick = function(e){
-        result.textContent = result.textContent + "9";
-    }
-    zero.onclick = function(e){
-        result.textContent = result.textContent + "0";
-    }
+    var mult = parseInt(n1) * parseInt(n2);
+    result.textContent = result.textContent + mult;
 
-    // Operaciones
-    sum.onclick = function(e){
-        number1 = result.textContent;
-        type_operation = "+";
-        clean_screen();
-    }
-
-    subt.onclick = function(e){
-        number1 = result.textContent;
-        type_operation = "-";
-        clean_screen();
-    }
-
-    multiplication.onclick = function(e){
-        number1 = result.textContent;
-        type_operation = "×";
-        clean_screen();
-    }
-
-    div.onclick = function(e){
-        number1 = result.textContent;
-        type_operation = "÷";
-        clean_screen();
-    }
-
-    clean.onclick = function(e){
-        reset();
-    }
-    
-    // Obteniendo el segundo número para la operación
-    same.onclick = function(e){
-        number2 = result.textContent;
-        solve();
-    }
 }
+
+
+
+
+
+
+
+
+// // Variables para la operaciones
+// var number1;
+// var number2;
+// var type_operation;
+
+// function inicio(){
+//     // Obteniendo los datos de los botonones para convertirlos en elementos
+//     var result = document.getElementById("result");
+//     var clean = document.getElementById("clean");
+//     var sum = document.getElementById("sum");
+//     var subt = document.getElementById("subt");
+//     var multiplication = document.getElementById("multiplication");
+//     var div = document.getElementById("div");
+//     var same = document.getElementById("same");
+//     var one = document.getElementById("one");
+//     var two = document.getElementById("two");
+//     var three = document.getElementById("three");
+//     var four = document.getElementById("four");
+//     var five = document.getElementById("five");
+//     var six = document.getElementById("six");
+//     var seven = document.getElementById("seven");
+//     var eight = document.getElementById("eight");
+//     var nine = document.getElementById("nine");
+//     var zero = document.getElementById("zero");
+
+//     // Eventos
+//     // Al presionar el número este se mostrará
+//     one.onclick = function(e){
+//         result.textContent = result.textContent + "1";
+//     }
+//     two.onclick = function(e){
+//         result.textContent = result.textContent + "2";
+//     }
+//     three.onclick = function(e){
+//         result.textContent = result.textContent + "3";
+//     }
+//     four.onclick = function(e){
+//         result.textContent = result.textContent + "4";
+//     }
+//     five.onclick = function(e){
+//         result.textContent = result.textContent + "5";
+//     }
+//     six.onclick = function(e){
+//         result.textContent = result.textContent + "6";
+//     }
+//     seven.onclick = function(e){
+//         result.textContent = result.textContent + "7";
+//     }
+//     eight.onclick = function(e){
+//         result.textContent = result.textContent + "8";
+//     }
+//     nine.onclick = function(e){
+//         result.textContent = result.textContent + "9";
+//     }
+//     zero.onclick = function(e){
+//         result.textContent = result.textContent + "0";
+//     }
+
+//     // Operaciones
+//     sum.onclick = function(e){
+//         number1 = result.textContent;
+//         type_operation = "+";
+//         clean_screen();
+//     }
+
+//     subt.onclick = function(e){
+//         number1 = result.textContent;
+//         type_operation = "-";
+//         clean_screen();
+//     }
+
+//     multiplication.onclick = function(e){
+//         number1 = result.textContent;
+//         type_operation = "×";
+//         clean_screen();
+//     }
+
+//     div.onclick = function(e){
+//         number1 = result.textContent;
+//         type_operation = "÷";
+//         clean_screen();
+//     }
+
+//     clean.onclick = function(e){
+//         reset();
+//     }
+    
+//     // Obteniendo el segundo número para la operación
+//     same.onclick = function(e){
+//         number2 = result.textContent;
+//         solve();
+//     }
+// }
